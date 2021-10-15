bc38394daec48803a51970c9910b388138409fa3
Georgie <gbenevento@cifpfbmoll.eu>
2021-10-15 20:49:26 +0200

    actualizacion repositorio

--- DWEC/ec02/ec02.js
+++ DWEC/ec02/ec02.js
@@ -15,25 +15,74 @@ function reverseString(){
 
     if (txt!==""){
         newTxt = txt.split("").reverse().join("");
-    }    
-
-    document.getElementById("answer").innerHTML = newTxt;
-    }
+        document.getElementById("answer").innerHTML = newTxt;
+    }else{
+        isEmpty();
+    }}
 
     function countA(){
 
     txt = document.getElementById("inp").value;
 
-    counter = 0;
-
     if (txt!==""){
-        for(i = 0; i = txt.length; i++){
-            if( txt.charAT(i)==="a"){
-                counter++
+
+        cont = 0;
+
+        for(i = 0; i < txt.length; i++){
+
+            if( txt.charAt(i).toLowerCase() === "a"){
+                cont++;
             }
+        }
+        
+        document.getElementById("answer").innerHTML = "There are "+ cont +" A's";
+
+    }else{
+        isEmpty(); 
+    }}
+
+    function firstA(){
+
+        txt = document.getElementById("inp").value;
+
+        if (txt !== ""){
+
+        if(txt.charAt(0)=== "a"){
+
+            document.getElementById("answer").innerHTML = "The first character is an 'A'";
+
+        }else{
+
+            count=0;
+
+            for (i = 1; i < (txt.length+1); i++) {
+
+                if(txt.charAt(i).toLowerCase() === "a"){
+
+                    newTxt = txt.substring(0,i);
+                    document.getElementById("answer").innerHTML = newTxt;
+                    i = txt.length;
+
+                }else{
+                    count++
+                }
+            }
+
+            if( count === txt.length){
+
+                document.getElementById("answer").innerHTML = "There are no A's"
+            }
+
+        }}else{
+            isEmpty();
         }
     }
 
-    document.getElementById("answer").innerHTML = "There are "+ counter + " A's";
-        
+    function firstAndLast(){
+
+        txt = document.getElementById("inp").value;
+
+        if (txt !== ""){
+            
+        }
     }
